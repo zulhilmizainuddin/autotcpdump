b34f14b4ecf41e131b3da3b7044a027be4a23603
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-06-12 09:24:33 +0800

    Allow passing of command line arguments to tcpdump

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
+	"strings"
 	"autotcpdump/parser"
 	"autotcpdump/executer"
 	"autotcpdump/checker"
 )
 
 func main() {
+	cmdlineArgs := os.Args[1:]
+
 	config := parser.ConfigParser{}
 	if err := config.Parse("config/config.json"); err != nil {
 		log.Fatal(err)
@@ -22,8 +26,10 @@ func main() {
 	filename := fmt.Sprintf("tcpdump_%v.pcap", time.Now().Format("20060102_150405"))
 	fmt.Println("directory:", config.PcapLocation, "filename:", filename)
 
+	commandOptions := config.CommandOptions + " " + strings.Join(cmdlineArgs, " ")
+
 	tcpdump := executer.TcpdumpExecuter{}
-	if err := tcpdump.RunTcpdump(config.PcapLocation, filename, config.CommandOptions); err != nil {
+	if err := tcpdump.RunTcpdump(config.PcapLocation, filename, commandOptions); err != nil {
 		log.Fatal(err)
 	}
 
